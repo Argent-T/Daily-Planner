6a0059b67d45ce713a66f54d9860372d11efb58c
JesterPanda <55069246+Jesterpanda@users.noreply.github.com>
2019-11-13 17:54:25 -0500

    initial timer

--- assets/planner.js
+++ assets/planner.js
@@ -1,6 +1,38 @@
-var currentDay = document.querySelector("#currentDay")
 
-var dateValue = moment().format("dddd, MMMM Do YYYY");
-console.log(dateValue)
+var nineA = document.querySelector("#nineA");
+var tenA = document.querySelector("#tenA");
+var elevenA = document.querySelector("#elevenA");
+var twelveP = document.querySelector("#twelveP");
+var oneP = document.querySelector("#oneP");
+var twoP = document.querySelector("#twoP");
+var threeP = document.querySelector("#threeP");
+var fourP = document.querySelector("#fourP");
+var fiveP = document.querySelector("#fiveP");
+var now = moment().format("h:mm a")
 
-currentDay.innerHTML = dateValue;
+
+var plans = {
+    nineAm: "",
+    tenAm: "",
+    elevenAm: "",
+    twelvePm: "",
+    onePm: "",
+    twoPm: "",
+    threePm: "",
+    fourPm: "",
+    fivePm: "",
+}
+
+
+document.querySelector("#currentDay")
+    .innerHTML = moment().format('MMMM Do YYYY, h:mm:ss a');
+
+
+var update = function () {
+    document.querySelector("#currentDay")
+        .innerHTML = moment().format('MMMM Do YYYY, h:mm:ss a');
+}
+setInterval(update, 1000);
+
+
+console.log(now)
