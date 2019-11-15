b6fe592fa5be665eb0c8fbba6fe8ca39d647c354
Thomas Argent <tfargent@gmail.com>
2019-11-15 15:27:11 -0500

    future

--- assets/planner.js
+++ assets/planner.js
@@ -37,7 +37,8 @@ times.addEventListener("click", function (event) {
 
 });
 
-
+//    var first = moment().hour(9)
+//    console.log(first.unix())
 
 // Time display////////////////////////
 document.querySelector("#currentDay")
@@ -45,47 +46,71 @@ document.querySelector("#currentDay")
 var update = function () {
     document.querySelector("#currentDay")
         .innerHTML = moment().format('MMMM Do YYYY, h:mm:ss a');
-        console.log("now: " +now)
-console.log("hour"+ moment().hour())
-    //    var first = moment().hour(9)
-    //    console.log(first.unix())
 
-        if(moment().hour() == 9){
-            nputs[0].className = "past present"
-        }
-        if(moment().hour() == 10){
-            inputs[0].className = "past"
-            inputs[1].className = "past present"
-        }
-        if(moment().hour() == 11){
-            inputs[1].className = "past"
-            inputs[2].className = "past present"
-        }
-        if(moment().hour() == 12){
-            inputs[2].className = "past"
-            inputs[3].className = "past present"
-        }
-        if(moment().hour() == 13){
-            inputs[3].className = "past"
-            inputs[4].className = "past present"
-        }
-        if(moment().hour() == 14){
-            inputs[4].className = "past"
-            inputs[5].className = "past present"
-        }
-        if(moment().hour() == 15){
-            inputs[5].className = "past"
-            inputs[6].className = "past present"
-        }
-        if(moment().hour() == 16){
-            inputs[6].className = "past"
-            inputs[7].className = "past present"
 
+
+    if (moment().hour() == 9) {
+        inputs[0].className = "past present"
+        for (i = 1; i < inputs.length; i++) {
+            inputs[i].className = "past future"
         }
-        if(moment().hour() == 17){
-            inputs[7].className = "past "
-            inputs[8].className = "past present"
+    }
+    if (moment().hour() == 10) {
+        inputs[0].className = "past"
+        inputs[1].className = "past present"
+        for (i = 2; i < inputs.length; i++) {
+            inputs[i].className = "past future"
         }
+    }
+    if (moment().hour() == 11) {
+        inputs[1].className = "past"
+        inputs[2].className = "past present"
+        for (i = 3; i < inputs.length; i++) {
+            inputs[i].className = "past future"
+        }
+    }
+    if (moment().hour() == 12) {
+        inputs[2].className = "past"
+        inputs[3].className = "past present"
+        for (i = 4; i < inputs.length; i++) {
+            inputs[i].className = "past future"
+        }
+    }
+    if (moment().hour() == 13) {
+        inputs[3].className = "past"
+        inputs[4].className = "past present"
+        for (i = 5; i < inputs.length; i++) {
+            inputs[i].className = "past future"
+        }
+    }
+    if (moment().hour() == 14) {
+        inputs[4].className = "past"
+        inputs[5].className = "past present"
+        for (i = 6; i < inputs.length; i++) {
+            inputs[i].className = "past future"
+        }
+    }
+    if (moment().hour() == 15) {
+        inputs[5].className = "past"
+        inputs[6].className = "past present"
+        for (i = 7; i < inputs.length; i++) {
+            inputs[i].className = "past future"
+        }
+
+    }
+    if (moment().hour() == 16) {
+        inputs[6].className = "past"
+        inputs[7].className = "past present"
+        for (i = 8; i < inputs.length; i++) {
+            inputs[i].className = "past future"
+        }
+
+    }
+    if (moment().hour() == 17) {
+        inputs[7].className = "past "
+        inputs[8].className = "past present"
+
+    }
 
 }
 setInterval(update, 1000);
@@ -93,5 +118,5 @@ setInterval(update, 1000);
 
 
 var now = moment().format("h:mm a");
-console.log("now: " +now)
-console.log("hour"+ moment().hour())
+console.log("now: " + now)
+console.log("hour" + moment().hour())
