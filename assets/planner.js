042ca6873793c407a982a9e685b73af96c44a04b
Thomas Argent <tfargent@gmail.com>
2019-11-15 15:13:35 -0500

    past and present

--- assets/planner.js
+++ assets/planner.js
@@ -8,8 +8,11 @@ var twoP = document.querySelector("#twoP");
 var threeP = document.querySelector("#threeP");
 var fourP = document.querySelector("#fourP");
 var fiveP = document.querySelector("#fiveP");
-
+var inputs = document.querySelectorAll(".input")
 var times = document.querySelector(".times");
+
+
+// queryselector all common class for array of input boxes
 
 
 var nineAm = [];
@@ -42,15 +45,53 @@ document.querySelector("#currentDay")
 var update = function () {
     document.querySelector("#currentDay")
         .innerHTML = moment().format('MMMM Do YYYY, h:mm:ss a');
+        console.log("now: " +now)
+console.log("hour"+ moment().hour())
+    //    var first = moment().hour(9)
+    //    console.log(first.unix())
 
-        if(moment().hour() === moment().hour(9)){
+        if(moment().hour() == 9){
+            nputs[0].className = "past present"
+        }
+        if(moment().hour() == 10){
+            inputs[0].className = "past"
+            inputs[1].className = "past present"
+        }
+        if(moment().hour() == 11){
+            inputs[1].className = "past"
+            inputs[2].className = "past present"
+        }
+        if(moment().hour() == 12){
+            inputs[2].className = "past"
+            inputs[3].className = "past present"
+        }
+        if(moment().hour() == 13){
+            inputs[3].className = "past"
+            inputs[4].className = "past present"
+        }
+        if(moment().hour() == 14){
+            inputs[4].className = "past"
+            inputs[5].className = "past present"
+        }
+        if(moment().hour() == 15){
+            inputs[5].className = "past"
+            inputs[6].className = "past present"
+        }
+        if(moment().hour() == 16){
+            inputs[6].className = "past"
+            inputs[7].className = "past present"
 
         }
-
-
+        if(moment().hour() == 17){
+            inputs[7].className = "past "
+            inputs[8].className = "past present"
+        }
 
 }
 setInterval(update, 1000);
 
+
+
 var now = moment().format("h:mm a");
-console.log(now)
+console.log("now: " +now)
+console.log("hour"+ moment().hour())
